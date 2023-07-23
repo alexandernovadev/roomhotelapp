3df67040f2a8b2c85caa3afa227b7eccacdc640e
alexandernovadev <alexsk88.dev@gmail.com>
2023-07-22 20:38:15 -0500

    [ADD] Vue App

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <button @click="handleToggle">Toogle</button>
+    <ModalJsx :show="show" message="HOla" /> -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+// import ModalJsx from "./render-functions/ModalJsx.vue";
+
+export default {
+  data() {
+    return {
+      show: false
+    };
+  },
+  components: {
+  },
+  methods: {
+    handleToggle() {
+      this.show = !this.show;
+    }
+  }
+};
+</script>
+<style lang="postcss" src="./assets/tailwind.postcss"></style>
+<style lang="css" src="./assets/main.css"></style>
---
+++ src/components/TinySlider.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="slider-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { tns } from 'tiny-slider/src/tiny-slider';
+import 'tiny-slider/dist/tiny-slider.css';
+
+export default {
+  name: 'TinySlider',
+  data() {
+      // El dolar al principio indica que no va a hacer reactivo 
+      // NO OBSERVABLE
+    return {
+      $slider: null,
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      // this.$el: este elemento
+      const settings = {
+        container: this.$el,
+        mode: 'gallery',
+      };
+      this.$slider = tns(settings);
+    },
+  },
+  beforeDestroy() {
+    if (this.$slider) {
+      this.$slider.destroy();
+    }
+  },
+};
+</script>
